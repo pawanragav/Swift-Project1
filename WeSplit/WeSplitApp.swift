60b09af1723d9efe9de9aa4dc89c13eb4ae77d27
Pawan Ragavendhar v <pawanharris@gmail.com>
2023-01-14 19:25:44 +0530

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Pawan's Mac on 14/01/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
